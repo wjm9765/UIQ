b137c3dbc14dbd490970dde46bed5ae34cbf7750
wjm <wjm9765@naver.com>
2026-04-08 16:21:06 +0000

    [FEAT] ADD extra config files for running time

---
+++ split_configs.py
@@ -0,0 +1,18 @@
+import sys
+from ruamel.yaml import YAML
+
+yaml = YAML(typ='safe')
+yaml.default_flow_style = False
+
+with open('config.yaml', 'r', encoding='utf-8') as f:
+    config = yaml.load(f)
+
+models = config.get('models', [])
+for m in models:
+    m['enabled'] = True
+    name = m['name']
+    new_conf = dict(config)
+    new_conf['models'] = [m]
+    
+    with open(f'config_{name}.yaml', 'w', encoding='utf-8') as fw:
+        yaml.dump(new_conf, fw)
